53c58ed0efe7a06fe8ccd0f412c4f05279099383
Aibek <aiba.prenov@gmail.com>
2025-03-20 15:57:34 +0500

    change ufw docker

--- handlers.py
+++ handlers.py
@@ -16,8 +16,8 @@ async def add(message, command):
     if not is_valid_ipv4_address(ip_address):
         await message.answer(f"Неверный IP-адрес {ip_address}")
     else:
-        response = subprocess.run(["ufw", "allow", "from", f"{ip_address}", "proto", "tcp", "to", "any", "port", "80"])
-        response1 = subprocess.run(["ufw", "allow", "from", f"{ip_address}", "proto", "tcp", "to", "any", "port", "443"])
+        response = subprocess.run(["ufw-docker", "allow", "from", f"{ip_address}", "proto", "tcp", "to", "any", "port", "80"])
+        response1 = subprocess.run(["ufw-docker", "allow", "from", f"{ip_address}", "proto", "tcp", "to", "any", "port", "443"])
 
         if response.returncode == 0 and response1.returncode == 0:
             await message.answer(f"IP-адрес {ip_address} добавлен в список разрешенных")

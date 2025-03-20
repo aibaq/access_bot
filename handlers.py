0448e7af2204eb12836ecfca22f36fc14944bdc1
Aibek <aiba.prenov@gmail.com>
2025-03-20 16:34:36 +0500

    fix response

--- handlers.py
+++ handlers.py
@@ -17,7 +17,7 @@ async def add(message, command):
         await message.answer(f"Неверный IP-адрес {ip_address}")
     else:
         response = subprocess.run(["iptables", "-I", "DOCKER-USER", "-p", "tcp", "--dport", "80", "-s", f"{ip_address}", "-j", "ACCEPT"])
-        response = subprocess.run(["iptables", "-I", "DOCKER-USER", "-p", "tcp", "--dport", "443", "-s", f"{ip_address}", "-j", "ACCEPT"])
+        response1 = subprocess.run(["iptables", "-I", "DOCKER-USER", "-p", "tcp", "--dport", "443", "-s", f"{ip_address}", "-j", "ACCEPT"])
 
         if response.returncode == 0 and response1.returncode == 0:
             await message.answer(f"IP-адрес {ip_address} добавлен в список разрешенных")

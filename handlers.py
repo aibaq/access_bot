283be3b2354cf46fc808e2420d4d9d00fd9f291a
Aibek <aiba.prenov@gmail.com>
2025-03-20 12:20:29 +0500

    remove ufw

--- handlers.py
+++ handlers.py
@@ -1,37 +1,25 @@
+import socket
+import subprocess
+
 from aiogram.filters.command import Command
 from aiogram import Router
-from aiogram.types import ChatMemberAdministrator, ChatMemberOwner, FSInputFile, BufferedInputFile
+
+from utils import is_valid_ipv4_address
 
 router = Router()
+
 
 @router.message(Command("add"))
 async def add(message, command):
     ip_address = command.args
 
+    if not is_valid_ipv4_address(ip_address):
+        await message.answer(f"Неверный IP-адрес {ip_address}")
+    else:
+        response = subprocess.run(["ufw", "allow", "from", f"{ip_address}", "proto", "tcp", "to", "any", "port", "80"])
+        response1 = subprocess.run(["ufw", "allow", "from", f"{ip_address}", "proto", "tcp", "to", "any", "port", "443"])
 
-@router.message(Command("open_access"))
-async def open_access(message, command):
-    import main
-    member = await main.bot.get_chat_member(message.chat.id, message.from_user.id)
-    docker_user_table = iptc.Table(iptc.Table.FILTER)
-    chain = iptc.Chain(docker_user_table, "DOCKER-USER")
-    reject_ip_addresses = [
-        rule for rule in chain.rules if
-        rule.out_interface and "br-456bf51da98b" in rule.out_interface and
-        rule.target and "REJECT" == rule.target.name
-    ]
-    try:
-        if not isinstance(member, (ChatMemberAdministrator, ChatMemberOwner)):
-            raise PermissionDenied
-        if not reject_ip_addresses:
-            raise IpAddressExistError
-        [chain.delete_rule(rule) for rule in reject_ip_addresses]
-        await message.answer(f"Доступ на сервер открыт всем")
-    except PermissionDenied:
-        await message.answer(f"Этот функционал доступен только для администраторов чата")
-    except IpAddressExistError:
-        await message.answer(f"Доступ на сервер уже открыт, проверьте доступ")
-
-
-# @router.message(Command("close_access"))
-# async def close_access(message, command):
+        if response.returncode == 0 and response1.returncode == 0:
+            await message.answer(f"IP-адрес {ip_address} добавлен в список разрешенных")
+        else:
+            await message.answer(f"Ошибка добавления IP-адреса {ip_address}")
--- main.py
+++ main.py
@@ -9,9 +9,11 @@ env = dotenv_values(".env")
 bot = Bot(token=env.get("BOT_TOKEN"))
 dp = Dispatcher()
 
+
 async def main():
     dp.include_router(router)
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
